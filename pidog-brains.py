c759b1689cdb7cf1043622c7d8c4509fe34b133e
Matt Moore <illogical@users.noreply.github.com>
2025-04-29 21:09:56 -0400

    Add service definition and wake up logic

--- pidog-brains.py
+++ pidog-brains.py
@@ -12,11 +12,7 @@ def main():
         if touch_status != 'N' and not touch_active:
             print("Touch detected!")
             touch_active = True
-            my_dog.speak('howling', 100)
-            my_dog.do_action('sit', speed=80)
-            my_dog.wait_all_done()
-            time.sleep(5)
-            my_dog.do_action('lie', speed=50)
+            wake_up(my_dog)
 
             #execute_command(my_dog, touch_status) # Execute command based on touch status
         elif touch_active:
@@ -24,10 +20,6 @@ def main():
             touch_active = False
             
         time.sleep(0.5)
-
-if __name__ == "__main__":
-    main()
-
 
 def execute_command(my_dog, command):
     # Placeholder for command execution logic
@@ -61,6 +53,18 @@ def execute_command(my_dog, command):
     #     case _:
     #         print("Unknown command.")
 
+def sample_howl(my_dog):
+    my_dog.speak('howling', 100)
+    my_dog.do_action('sit', speed=80)
+    my_dog.head_move([[0, 0, -30]], speed=70)
+    my_dog.wait_all_done()
+    time.sleep(3)
+    my_dog.do_action('lie', speed=50)
+    my_dog.wait_all_done()
+    my_dog.speak('snoring', 100)
+    my_dog.wait_all_done()
+    time.sleep(2)
+
 def howling(my_dog, volume=100):
     my_dog.do_action('sit', speed=80)
     my_dog.head_move([[0, 0, -30]], speed=95)
@@ -79,7 +83,47 @@ def howling(my_dog, volume=100):
     my_dog.head_move([[0, 0, 10]], speed=80)
     my_dog.wait_all_done()
 
-    sleep(2.34)
+    time.sleep(2.34)
     my_dog.do_action('sit', speed=80)
     my_dog.head_move([[0, 0, -40]], speed=80)
     my_dog.wait_all_done()
+
+def wake_up(my_dog):
+    # stretch
+    my_dog.rgb_strip.set_mode('listen', color='yellow', bps=0.6, brightness=0.8)
+    my_dog.do_action('stretch', speed=50)
+    my_dog.head_move([[0, 0, 30]]*2, immediately=True)
+    my_dog.wait_all_done()
+    time.sleep(0.2)
+    my_dog.wait_all_done()
+    time.sleep(0.5)
+    my_dog.head_move([[0, 0, -30]], immediately=True, speed=70)
+    # sit and wag_tail
+    my_dog.do_action('sit', speed=25)
+    my_dog.wait_legs_done()
+    time.sleep(0.5)
+    my_dog.head_move([[-20, 15, -20]], speed=60)
+    my_dog.wait_all_done()
+    time.sleep(2)
+    my_dog.head_move([[0, 0, 0]], speed=70)
+    time.sleep(1)
+    my_dog.wait_all_done()
+    my_dog.rgb_strip.set_mode('speak', color='cyan', bps=0.6)
+    my_dog.do_action('half_sit', speed=80)
+    my_dog.speak('howling', 100)
+    my_dog.head_move([[0, 0, 20]], speed=80)
+    my_dog.do_action('wag_tail', step_count=10, speed=100)
+    my_dog.rgb_strip.set_mode('breath', color=[245, 10, 10], bps=2.5, brightness=0.8)
+    my_dog.wait_all_done()
+    time.sleep(0.5)
+    my_dog.head_move([[0, 0, -20]], speed=50)
+    # hold
+    my_dog.do_action('wag_tail', step_count=10, speed=80)
+    my_dog.rgb_strip.set_mode('breath', 'pink', bps=0.5)
+    my_dog.wait_all_done()
+    time.sleep(2)
+    my_dog.stop_and_lie()
+
+
+if __name__ == "__main__":
+    main()
---
+++ start-pidog-brains.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the script's directory
+cd /c/SynologyDrive/Drive/Development/RaspPi/Pidog
+
+# Start the Python script
+/usr/bin/python3 pidog-brains.py
