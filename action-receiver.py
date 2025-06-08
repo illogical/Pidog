b859152e84585c88ac5130f0229ac46de09669ae
Matt Moore <illogical@users.noreply.github.com>
2025-06-08 17:38:07 -0400

    Refactor RabbitMQ connection to use environment variables for configuration

--- action-receiver.py
+++ action-receiver.py
@@ -1,16 +1,28 @@
 # This script receives actions from a RabbitMQ queue
 """Blocking consumer for messages published."""
 import pika
+from dotenv import load_dotenv
+import os
 
-HOST  = "beast2024.bangus-city.ts.net"
-QUEUE = "pidog-actions"
 
 def callback(ch, method, properties, body):
     print(f" [x] Received {body.decode()}")
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 if __name__ == "__main__":
-    params     = pika.ConnectionParameters(host=HOST)
+    load_dotenv()
+    # RabbitMQ configuration
+    USERNAME = os.getenv("RABBITMQ_USERNAME")
+    PASSWORD = os.getenv("RABBITMQ_PASSWORD")
+    HOST = os.getenv("RABBITMQ_HOST")
+    QUEUE = os.getenv("RABBITMQ_QUEUE")
+
+    if None in (USERNAME, PASSWORD, HOST, QUEUE):
+        raise ValueError("One or more RabbitMQ environment variables are not set.")
+    
+    # Establish connection to RabbitMQ
+    credentials = pika.PlainCredentials(USERNAME, PASSWORD)
+    params     = pika.ConnectionParameters(host=HOST, credentials=credentials)
     connection = pika.BlockingConnection(params)
     channel    = connection.channel()
 
